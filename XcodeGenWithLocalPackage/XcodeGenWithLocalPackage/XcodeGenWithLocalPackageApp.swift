bdcb6233b3de4f689e2c7414274f6ab738fc9a28
takehilo <takehilo.kaneko@gmail.com>
2024-03-27 23:40:23 +0900

    add initial XcodeGenWithLocalPackage

---
+++ XcodeGenWithLocalPackage/XcodeGenWithLocalPackage/XcodeGenWithLocalPackageApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGenWithLocalPackageApp.swift
+//  XcodeGenWithLocalPackage
+//
+//  Created by takehiro.kaneko on 2024/03/27.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGenWithLocalPackageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
