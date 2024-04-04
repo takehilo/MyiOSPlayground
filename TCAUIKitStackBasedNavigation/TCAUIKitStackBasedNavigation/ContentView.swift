4093e2e7f468a2615c414abd08ba31ccf1bd457d
takehilo <takehilo.kaneko@gmail.com>
2024-04-04 09:50:03 +0900

    add initial TCAUIKitStackBasedNavigation

---
+++ TCAUIKitStackBasedNavigation/TCAUIKitStackBasedNavigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCAUIKitStackBasedNavigation
+//
+//  Created by takehiro.kaneko on 2024/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCAUIKitStackBasedNavigation/TCAUIKitStackBasedNavigation/TCAUIKitStackBasedNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAUIKitStackBasedNavigationApp.swift
+//  TCAUIKitStackBasedNavigation
+//
+//  Created by takehiro.kaneko on 2024/04/04.
+//
+
+import SwiftUI
+
+@main
+struct TCAUIKitStackBasedNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
